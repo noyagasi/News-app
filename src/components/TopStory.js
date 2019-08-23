19b87634792e2e6f0e2ce0dac15fc0b92ab2e762
Segev Haviv <segevhaviv@gmail.com>
2019-08-23 18:40:59 +0300

    Suggestions

--- src/components/TopStory.js
+++ src/components/TopStory.js
@@ -1,26 +1,27 @@
-import React, { Component } from 'react';
-import { Text, TouchableOpacity } from 'react-native';
-import axios from 'axios';
-
-
+import React, { Component } from "react";
+import { Text } from "react-native";
+import axios from "axios";
+import LoadingIndicator from "./LoadingIndicator";
 class TopStory extends Component {
-   state = { title: '' };
-   componentWillMount () {
-     axios.get('https://hacker-news.firebaseio.com/v0/item/' + this.props.storyID + '.json')
-     .then(response => this.setState({title: response.data.title }));
-   }
-  render () {
-    return (
-       <Text> { this.state.title } </Text>
-    );
+  state = { title: "" };
+
+  componentWillMount() {
+    const { storyID } = this.props;
+
+    axios
+      .get("https://hacker-news.firebaseio.com/v0/item/" + storyID + ".json")
+      .then(response => this.setState({ title: response.data.title }));
+  }
+  render() {
+    const { title } = this.state;
+    return title ? <Text> {title} </Text> : <LoadingIndicator />;
   }
 }
 
-
-const styles = {
+const styles = StyleSheet.create({
   headerContentStyle: {
-    flexDirection: 'column',
-    justifyContent: 'space-around'
+    flexDirection: "column",
+    justifyContent: "space-around"
   },
   headerTextStyle: {
     fontSize: 18
@@ -30,8 +31,8 @@ const styles = {
     width: 50
   },
   thumbnailContainerStyle: {
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: "center",
+    alignItems: "center",
     marginLeft: 10,
     marginRight: 10
   },
@@ -40,6 +41,6 @@ const styles = {
     flex: 1,
     width: null
   }
-};
+});
 
 export default TopStory;
